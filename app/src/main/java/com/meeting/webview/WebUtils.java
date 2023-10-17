353432a143066f4428e2c5a245a839c17a806947
Enablex <pankaj.p@vcloudx.com>
2023-10-17 10:13:00 +0530

    added attachment and download listner

--- app/src/main/java/com/meeting/webview/WebUtils.java
+++ app/src/main/java/com/meeting/webview/WebUtils.java
@@ -1,6 +1,8 @@
 package com.meeting.webview;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
+import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -13,7 +15,19 @@ public class WebUtils {
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setDatabaseEnabled(true);
+        webSettings.setSupportMultipleWindows(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setMixedContentMode(0);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setCacheMode(webSettings.LOAD_NO_CACHE);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         webSettings.setMediaPlaybackRequiresUserGesture(false);
+        if (Build.VERSION.SDK_INT >= 19) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+        else if(Build.VERSION.SDK_INT >=11 && Build.VERSION.SDK_INT < 19) {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
         CookieManager.getInstance().setAcceptCookie(true);
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
     }
